6d8e06b0a6989ef2b51e68709f3ed82530d299b1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+// ...
+
+namespace DrillThroughReport {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            new MasterReport().ShowPreview();
+        }
+    }
+}
